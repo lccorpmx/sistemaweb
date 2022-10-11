fee5582997c2217caa004789097b4a6a98df2a53
Arturo Zilli Rios <111665236+lccorpmx@users.noreply.github.com>
2022-10-11 16:58:41 -0500

    Update models.py

--- questions/models.py
+++ questions/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from django.conf import settings
+# ...code
 
 # Create your models here.
-
 class Question(models.Model):
     description = models.TextField(blank=True)
+    posted_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
+
+class Vote(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    question = models.ForeignKey('questions.Question', related_name='votes', on_delete=models.CASCADE)
